5a2f55e6a85e46708cfc84da7b07b8ff18ced1dc
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-12 23:37:29 +0100

    FEAT: Number Guessing Game

--- number-guessing-game/number-guessing-number.c
+++ number-guessing-game/number-guessing-number.c
@@ -1,4 +1,32 @@
-int main(void){
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
     const int number = 97;
-    
+
+    printf("Guess the number!\n");
+    printf("Enter a number between 1 and 100 or 'q' to quit\n");
+
+    int guess = 0;
+    while (guess != number) {
+        char input[10];
+        printf("Your guess: ");
+        fgets(input, 10, stdin);
+
+        if (input[0] == 'q') {
+            printf("Quitting...\n");
+            exit(0);
+        }
+
+        guess = atoi(input);
+
+        if (guess < number) {
+            printf("Too low!\n");
+        } else if (guess > number) {
+            printf("Too high!\n");
+        } else {
+            printf("Correct!\n");
+        }
+    }
+
 }
