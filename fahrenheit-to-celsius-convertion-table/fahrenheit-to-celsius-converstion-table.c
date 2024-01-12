dbe3d5d2a1b3ac0aefbf2955a5b6b8c23565889b
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-12 23:46:41 +0100

    FEAT Implement reverse parameter for the temperature conversion table

--- fahrenheit-to-celsius-convertion-table/fahrenheit-to-celsius-converstion-table.c
+++ fahrenheit-to-celsius-convertion-table/fahrenheit-to-celsius-converstion-table.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
-void convertionTabletFromFahrenheitToCelcius(void) {
+void convertionTabletFromFahrenheitToCelcius(const bool r) {
     const int upper = 300;
     const int lower = 0;
     int fahr = lower;
     printf("Temperature conversion table\n");
     printf("\nFº\tCº\n");
+
+    // If r == true, the table will be printed in reverse order
+    if (r) {
+        fahr = upper;
+        while (fahr >= lower) {
+            const int step = 20;
+            const int celsius = 5 * (fahr - 32) / 9;
+            printf("%d\t%d\n", fahr, celsius);
+            fahr = fahr - step;
+        }
+        return;
+    }
     while (fahr <= upper) {
         const int step = 20;
         const int celsius = 5 * (fahr - 32) / 9;
@@ -14,12 +26,24 @@ void convertionTabletFromFahrenheitToCelcius(void) {
     }
 }
 
-void convertionTabletFromCelciusToFahrenheit(void) {
+void convertionTabletFromCelciusToFahrenheit(const bool r) {
     const int upper = 300;
     const int lower = 0;
     int celsius = lower;
     printf("Temperature conversion table\n");
     printf("\nCº\tFº\n");
+
+    // if r == true, the table will be printed in reverse order
+    if (r) {
+        celsius = upper;
+        while (celsius >= lower) {
+            const int step = 20;
+            const int fahr = (9 * celsius) / 5 + 32;
+            printf("%d\t%d\n", celsius, fahr);
+            celsius = celsius - step;
+        }
+        return;
+    }
     while (celsius <= upper) {
         const int step = 20;
         const int fahr = (9 * celsius) / 5 + 32;
@@ -29,8 +53,10 @@ void convertionTabletFromCelciusToFahrenheit(void) {
 }
 
 int main() {
-    convertionTabletFromFahrenheitToCelcius();
+    convertionTabletFromFahrenheitToCelcius(0);
     printf("\n");
-    convertionTabletFromCelciusToFahrenheit();
+    convertionTabletFromCelciusToFahrenheit(0);
+    printf("\n");
+    convertionTabletFromFahrenheitToCelcius(1);
     return 0;
 }
