7af49abe656fc4bc04cf4cfd1689f77f7c35abc0
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-19 00:31:24 +0100

    FEAT: Moon Phase Indicator

--- greetings/greetings.c
+++ greetings/greetings.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
 #include <time.h>
 
+
+int moon_phase (int year, int month, int day) {
+
+  // int d;
+  int g, e;
+
+  // d = day;
+
+  // // Convert month number to be 3..14 (March=3, April=4, etc.)
+  // if (month == 2) {
+  //   d += 31;
+  // } else if (month > 3) {
+  //   d += 59 + (month - 3)*30.6 +  0.5;
+  // }
+
+  // Separate out centuries and years
+  g = (year - 1900) % 19;
+  e = (11*g + 29) % 30;
+
+  // Account for leap year
+  if ((e == 25 && g > 11) || e == 24) {
+    e++;
+  }
+
+  // Find full moon's calendar date
+  return ((((e + 1) * 6+5) % 177) / 22 & 7);
+}
+
+char *get_moon_phase_name (int phase) {
+  char *phase_name;
+  switch (phase) {
+    case 0:
+      phase_name = "new moon";
+      break;
+    case 1:
+      phase_name = "waxing crescent moon";
+      break;
+    case 2:
+      phase_name = "first quarter moon";
+      break;
+    case 3:
+      phase_name = "waxing gibbous moon";
+      break;
+    case 4:
+      phase_name = "full moon";
+      break;
+    case 5:
+      phase_name = "waning gibbous moon";
+      break;
+    case 6:
+      phase_name = "last quarter moon";
+      break;
+    case 7:
+      phase_name = "waning crescent moon";
+      break;
+    default:
+      phase_name = "ERROR";
+      break;
+  }
+  return phase_name;
+}
+
+
 int main (int argc, char *argv[]) {
   time_t now ;
   struct tm *clock;
@@ -9,6 +72,9 @@ int main (int argc, char *argv[]) {
   time(&now);
   clock = localtime(&now);
   hour = clock -> tm_hour;
+
+  int phase = moon_phase(clock -> tm_year + 1900, clock -> tm_mon + 1, clock -> tm_mday);
+  char *phase_name = get_moon_phase_name(phase);
 
   strftime(time_string,64,"Today is %A, %B %d, %Y%nIt is %r%n",clock);
 
@@ -31,8 +97,7 @@ int main (int argc, char *argv[]) {
     printf("code master!\n");
   }
   printf("%s", time_string);
-
-
+  printf("Tonight we have a %s on the sky.\n", phase_name);
 
   return 0;
 }
