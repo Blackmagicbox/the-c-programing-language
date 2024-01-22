6796d1828341266daa5e8e441ac0692bd557f889
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-22 23:08:50 +0100

    Nato phonetic alphabet translator (#1)
    * CHORE: Refactor phonetic alphabet translator [DRAFT]
    
    * CHORE: Refactor the translation method
    
    * CHORE: refactor replacement loop
    
    * FEAT: Reading file and translating file to Nato Phonetic Alphabet

--- nato_phonetical_alphabet/nato_phonetical_alphabet.c
+++ nato_phonetical_alphabet/nato_phonetical_alphabet.c
@@ -5,39 +5,50 @@
 #define ASCII_UPPER_CASE_A 65
 #define ASCII_UPPER_CASE_Z 90
 #define ALPHABET_LENGTH 26
-#define PHONETIC_INDEX_LENGTH 124
 #define ASCII_UPPER_CASE_TO_LOWER_CASE_DIFFERENCE 32
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
-  char natoPhoneticAlphabet[ALPHABET_LENGTH][8] = {
-      "Alpha","Bravo","Charlie","Delta",
-      "Echo","Foxtrot","Golf","Hotel",
-      "India","Juliett","Kilo","Lima",
-      "Mike","November","Oscar","Papa",
-      "Quebec","Romeo","Sierra","Tango",
-      "Uniform","Victor","Whiskey","X-ray",
-      "Yankee","Zulu"
+  char nato[ALPHABET_LENGTH][8] = {
+      "Alpha", "Bravo", "Charlie", "Delta",
+      "Echo", "Foxtrot", "Golf", "Hotel",
+      "India", "Juliett", "Kilo", "Lima",
+      "Mike", "November", "Oscar", "Papa",
+      "Quebec", "Romeo", "Sierra", "Tango",
+      "Uniform", "Victor", "Whiskey", "X-ray",
+      "Yankee", "Zulu"
   };
 
 
-  char input_char;
+  FILE *n;
 
-  // Get input_char from input
-  printf("Enter a string: ");
+  char ch;
 
-  while ((input_char = getchar()) != '\n') {
-    if (input_char >= ASCII_UPPER_CASE_A && input_char <= ASCII_UPPER_CASE_Z) {
-      input_char = input_char + ASCII_UPPER_CASE_TO_LOWER_CASE_DIFFERENCE;
+  if (argc < 2) {
+    printf(stderr, "Provide a file name\n");
+    return 1;
+  }
+
+  n = fopen(argv[1], "r");
+  if(n == NULL) {
+    printf("Error opening file\n");
+    return 1;
+  }
+
+  while ((ch = fgetc(n)) != EOF) {
+    if (ch >= ASCII_UPPER_CASE_A && ch <= ASCII_UPPER_CASE_Z) {
+      ch = ch + ASCII_UPPER_CASE_TO_LOWER_CASE_DIFFERENCE;
     }
-    if (input_char >= ASCII_LOWER_CASE_A && input_char <= ASCII_LOWER_CASE_Z) {
-      printf("%s-", natoPhoneticAlphabet[input_char - ASCII_LOWER_CASE_A]);
+    if (ch >= ASCII_LOWER_CASE_A && ch <= ASCII_LOWER_CASE_Z) {
+      printf("%s-", nato[ch - ASCII_LOWER_CASE_A]);
     } else {
-      putchar(input_char);
+      putchar(ch);
     }
   }
   printf("\n");
+
+  fclose(n);
 
   return 0;
 }
