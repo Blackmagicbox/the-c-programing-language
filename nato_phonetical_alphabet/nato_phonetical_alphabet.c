112c9f4ae4b907c1c5a9ef774170813efdf6c24b
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-22 22:45:25 +0100

    CHORE: refactor replacement loop

--- nato_phonetical_alphabet/nato_phonetical_alphabet.c
+++ nato_phonetical_alphabet/nato_phonetical_alphabet.c
@@ -5,60 +5,38 @@
 #define ASCII_UPPER_CASE_A 65
 #define ASCII_UPPER_CASE_Z 90
 #define ALPHABET_LENGTH 26
-#define PHONETIC_INDEX_LENGTH 124
 #define ASCII_UPPER_CASE_TO_LOWER_CASE_DIFFERENCE 32
 
 
 int main() {
-  char nato[ALPHABET_LENGTH][8] = {
-      "Alpha",
-      "Bravo",
-      "Charlie",
-      "Delta",
-      "Echo",
-      "Foxtrot",
-      "Golf",
-      "Hotel",
-      "India",
-      "Juliett",
-      "Kilo",
-      "Lima",
-      "Mike",
-      "November",
-      "Oscar",
-      "Papa",
-      "Quebec",
-      "Romeo",
-      "Sierra",
-      "Tango",
-      "Uniform",
-      "Victor",
-      "Whiskey",
-      "X-ray",
-      "Yankee",
-      "Zulu"
+
+  char natoPhoneticAlphabet[ALPHABET_LENGTH][8] = {
+      "Alpha", "Bravo", "Charlie", "Delta",
+      "Echo", "Foxtrot", "Golf", "Hotel",
+      "India", "Juliett", "Kilo", "Lima",
+      "Mike", "November", "Oscar", "Papa",
+      "Quebec", "Romeo", "Sierra", "Tango",
+      "Uniform", "Victor", "Whiskey", "X-ray",
+      "Yankee", "Zulu"
   };
 
-  char phrase[64];
-  char ch = ' ';
 
-  
-  printf("Enter a word or phrase: ");
-  fgets(phrase,64, stdin);
+  char input_char;
 
+  // Get input_char from input
+  printf("Enter a string: ");
 
-  int i = 0;
-  while(phrase[i]) {
-    ch = toupper(phrase[i]);
-    if(isalpha(ch)) {
-      printf("%s-", nato[ch - 'A']);
+  while ((input_char = getchar()) != '\n') {
+    if (input_char >= ASCII_UPPER_CASE_A && input_char <= ASCII_UPPER_CASE_Z) {
+      input_char = input_char + ASCII_UPPER_CASE_TO_LOWER_CASE_DIFFERENCE;
     }
-    i++;
-    if(i== 64) {
-      break;
+    if (input_char >= ASCII_LOWER_CASE_A && input_char <= ASCII_LOWER_CASE_Z) {
+      printf("%s-", natoPhoneticAlphabet[input_char - ASCII_LOWER_CASE_A]);
+    } else {
+      putchar(input_char);
     }
   }
-  putchar('\n');
-  
-  return (0);
+  printf("\n");
+
+  return 0;
 }
