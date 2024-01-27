3898cbc43acd3a5370ce910f4673214e30392712
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-28 00:58:43 +0100

    FEAT File output of encrypted text

--- caesarean_cypher/caesarean_cypher.c
+++ caesarean_cypher/caesarean_cypher.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
-int main (int charc, char *charv[]) {
+
+int main (int argc, char *argv[]) {
   printf("Type your secret message: ");
+
+  char filename[64] = "./encrypted.txt";
+
+  if(argc > 1)
+    strcpy(filename, argv[1]);
+
+  int fp = open(filename,O_RDWR | O_CREAT, 0644 );
+  if(fp == -1) {
+    perror("open");
+    return 1;
+  } 
   
   char message[100];
   char ch;
@@ -23,6 +38,10 @@ int main (int charc, char *charv[]) {
   }
 
   message[idx] = '\0';
+
+  write(fp, message, strlen(message));
+  
+  close(fp);
 
   printf("Encrypted message: %s\n", message);
   return 0; 
