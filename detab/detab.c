9a07460f2c6dcd8b26f21771b6a2807d621d1ad3
Rafael Santos <rafa@blackmagicbox.com.br>
2024-02-07 01:03:57 +0100

    Extract the Tabspaces to a simbolic parameter

--- detab/detab.c
+++ detab/detab.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+#define TAB_SPACES 2
+
+void add_spaces (void) {
+  for(int i = 0; i < TAB_SPACES; i++) {
+    putchar(' ');
+  }
+}
+
 int main(void) {
   int ch;
 
-  printf("Type a word: ");
+  printf("Type a word:\n");
 
   while((ch = getchar()) != EOF) {
     if(ch == '\t') {
-      putchar(' ');
-      putchar(' ');
+      add_spaces();
     } else {
       putchar(ch);
     }
