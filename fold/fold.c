ff3ccd267cd27019b6bb9015f76dc0a8aa7e94db
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-02-22 00:14:23 +0100

    Feat getLine Method

--- fold/fold.c
+++ fold/fold.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+#define MAX_LINE_LENGTH 1000
+#define FOLD_LENGTH 80
+
+int getLine (char line[], int maxLine) {
+  int c = '\b';
+  int i;
+
+  for (i = 0; i < maxLine -1 && (c = getchar()) != EOF && c != '\n' ; i++) {
+    line[i] = c;
+  }
+  if (c == '\n') {
+    line[i] = c;
+    i++;
+  }
+  line[i] = '\0';
+  return i;
+}
+
 
 int main(void) {
   printf("Hello Fold!\n");
