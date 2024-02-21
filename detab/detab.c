36960efc97fbe1690eb25e6a4266ae617ea095af
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-02-21 18:28:14 +0100

    CHORE Improve code styling

--- detab/detab.c
+++ detab/detab.c
@@ -1,62 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_BUFFER   1024
-#define SPACE        ' '
-#define TAB_CHARACTER          '\t'
+#define MAX_BUFFER 1024
+#define SPACE ' '
+#define TAB_CHARACTER '\t'
+#define TAB_OFFSET 2
 
-int CalculateNumberOfSpaces(int Offset, int TabSize) {
-  return TabSize - (Offset % TabSize);
-}
-
-int get_line(char line[], int limit) {
-  int ch, i;
-
-  // Read characters into line[] until limit is reached, EOF is encountered, or a newline ch is read
-  for (i = 0; i < limit - 1 && (ch = getchar()) != EOF && ch != '\n'; ++i) {
-    line[i] = ch;
-  }
-
-  // If a newline ch was read, add it to the line
-  if (ch == '\n') {
-    line[i] = ch;
-    ++i;
-  }
-
-  // Null-terminate the line
-  line[i] = '\0';
-
-  // Return the length of the line
-  return i;
-}
+void deSpacer(char *buffer);
 
 int main(int argc, char *argv[]) {
-  printf("Type a phrase\n");
-  char Buffer[MAX_BUFFER];
-  // Set the tab size based on the command line argument, or default to 4
-  int TabSize = argv[1] ? atoi(argv[1]) : 4;
+  // # Take the number of spaces and replace by the minimum number of tab characters necessary to keep the same space between words
+  // Get each character on a line
+  // Save to the buffer
+  // iterate over the buffer printing/putting each character
+  // Whenever a space appears:
+  // Start counting (start a counter)
+  // Whenever the number of spaces count matches the offset of the tab character.
+  // Put, print a tabCharacter.
+  // Set the Space counter to zero.
+  // Keep iterating and printing.
+  // Edge cases:
+  // If the first Character of a line is a space, Ignore it.
+  // If the number of spaces between two words is less than the offset of the Tab Character, print the correct amount of spaces instead.
+  // If the space between two words is bigger than the offset, print/put as many tab characters you can fit preserving the space and whatever is left put the appropriate number of spaces.
 
-  int ch, numSpaces, spaceIndex, outputLength;
 
-  // Process each line of input
-  while (get_line(Buffer, MAX_BUFFER) > 0) {
-    // Process each character in the line
-    for (ch = 0, outputLength = 0; Buffer[ch] != '\0'; ch++) {
-      if (Buffer[ch] == TAB_CHARACTER) {
-        // If the character is a tab, calculate the number of spaces to output
-        numSpaces = CalculateNumberOfSpaces(outputLength, TabSize);
-        // Output the calculated number of spaces
-        for (spaceIndex = 0; spaceIndex < numSpaces; spaceIndex++) {
-          putchar(SPACE);
-          outputLength++;
-        }
+  // Getting the line.
+
+  int buffer[MAX_BUFFER];
+  int ch = '\b';
+  int idx = 0;
+
+  while ((ch = getchar()) != EOF) {
+    if (ch == '\n' || idx >= MAX_BUFFER) {
+      //Call the De-spacer method passing the Buffer as parameter
+      deSpacer(buffer);
+      idx = 0;
+    } else {
+      // Check if the buffer is in the last position
+      if (idx == MAX_BUFFER - 1) {
+       buffer[idx] = '\0';
+       idx = 0;
+       continue;
       } else {
-        // If the character is not a tab, output it as is
-        putchar(Buffer[ch]);
-        outputLength++;
+        buffer[0] = ch;
+        idx++;
       }
     }
   }
 
+
+
   return 0;
+}
+
+void deSpacer(char *buffer){
+  int acc = 0;
+  // Iterate over the buffer by shifting the pointer
+  while(*buffer != '\0') {
+    if (*buffer == SPACE) {
+      if (acc >= TAB_OFFSET) {
+        putchar(TAB_CHARACTER);
+        acc = 0;
+      }
+      acc++;
+    } else {
+      for (int i = acc; i > 0; i--) {
+        putchar(SPACE);
+      }
+      putchar(*buffer);
+    }
+    *buffer++;
+  }
+  putchar('\n');
 }
