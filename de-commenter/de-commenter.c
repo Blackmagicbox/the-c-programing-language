005ca83d8624cd21e339e4aca7f2651514e17e8e
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-02-24 20:30:19 +0100

    CHORE: Extract literal characters to Symbos

--- de-commenter/de-commenter.c
+++ de-commenter/de-commenter.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 
 #define MAX_LINE_BUFFER 1024
+#define LINE_BREAK '\n'
+#define COMMENT_STARTER_CHAR '/'
+#define MULTI_LINE_COMMENT_MARKER '*'
 
 int main(int argc, char *argv[]) {
   int ch;
   char buffer[MAX_LINE_BUFFER];
   char *p = buffer;
-  int comment_start = 0;
+  int start_comment = 0;
   int in_single_line_comment = 0;
   int in_multi_line_comment = 0;
 
@@ -26,20 +29,20 @@ int main(int argc, char *argv[]) {
   }
 
   while ((ch = fgetc(fp)) != EOF) {
-    if (ch != '\n') {
-      if (ch == '/') {
+    if (ch != LINE_BREAK) {
+      if (ch == COMMENT_STARTER_CHAR) {
         if (!in_multi_line_comment) {
-          if (comment_start) {
+          if (start_comment) {
             in_single_line_comment = 1;
           } else {
-            comment_start = 1;
+            start_comment = 1;
           }
         } else {
           in_multi_line_comment = 0;
         }
-      } else if (ch == '*' && comment_start) {
+      } else if (ch == MULTI_LINE_COMMENT_MARKER && start_comment) {
         in_multi_line_comment = 1;
-      } else if (!in_single_line_comment && !in_multi_line_comment && !comment_start) {
+      } else if (!in_single_line_comment && !in_multi_line_comment && !start_comment) {
         *p = (char) ch;
         p++;
       }
@@ -49,7 +52,7 @@ int main(int argc, char *argv[]) {
       fprintf(op, "%s\n", buffer);
       p = buffer;
       if (!in_multi_line_comment) {
-        comment_start = 0;
+        start_comment = 0;
       }
       in_single_line_comment = 0;
     }
