a5df6bd45d44500fd3ab7ef5eded55750e44b0f6
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-02-24 18:45:53 +0100

    Feat De-commenter Feature [DRAFT] - Removes single line comments

--- de-commenter/de-commenter.c
+++ de-commenter/de-commenter.c
@@ -1,53 +1,36 @@
 #include <stdio.h>
 
-#define NEWLINE '\n'
-#define SINGLE_COMMENT_START '/'
-#define MULTILINE_COMMENT_START '/'
-#define MULTILINE_COMMENT_END '*'
+#define MAX_LINE_BUFFER 1024
 
 int main(void) {
-  int current_char;
-  int in_single_comment = 0;
-  int in_multiline_comment = 0;
+  // Read A line.
+  int ch;
+  char buffer[MAX_LINE_BUFFER];
+  char *p = buffer;
+  int single_line_comment_start = 0;
+  int in_single_line_comment = 0;
 
-
-  FILE *input = fopen("input.c", "r");
-  FILE *output = fopen("output.c", "w");
-
-  if (input == NULL || output == NULL) {
-    printf("Error opening file!\n");
-    return 1;
-  }
-
-  while ((current_char = fgetc(input)) != EOF) {
-    if (!in_single_comment && !in_multiline_comment && current_char == SINGLE_COMMENT_START) {
-      in_single_comment = 1;
-      continue;
-    } else if (in_single_comment && current_char == NEWLINE) {
-      in_single_comment = 0;
-    }
-    if (!in_single_comment && !in_multiline_comment && current_char == MULTILINE_COMMENT_START) {
-      in_multiline_comment = 1;
-      current_char = fgetc(input);
-      if (current_char != MULTILINE_COMMENT_END) {
-        printf("Error: Multiline comment not closed properly!\n");
-        continue;
+  while ((ch = getchar()) != EOF) {
+    if(ch !='\n') {
+      if (ch == '/') {
+        if (single_line_comment_start) {
+          in_single_line_comment = 1;
+        } else {
+          single_line_comment_start = 1;
+        }
+      } else if (!in_single_line_comment) {
+        *p = (char) ch;
+        p++;
       }
-    } else if (in_multiline_comment && current_char == MULTILINE_COMMENT_END) {
-      current_char = fgetc(input);
-      if (current_char != MULTILINE_COMMENT_START) {
-        printf("Error: Multiline comment not closed properly!\n");
-        continue;
-      }
-      in_multiline_comment = 0;
-    }
-    if (!in_single_comment && !in_multiline_comment) {
-      fputc(current_char, output);
+    } else {
+      *p = '\0';
+      printf("%s\n", buffer);
+      p = buffer;
+      single_line_comment_start = 0;
+      in_single_line_comment = 0;
     }
   }
-
-  fclose(input);
-  fclose(output);
 
   return 0;
+
 }
